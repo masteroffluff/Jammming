a420c66718dd9db7e8f7a53f424e7baf9e6efb59
masteroffluff <chris.chapman@linuxmail.org>
2023-09-19 23:06:50 +0100

    day 3+

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SearchResults from './SearchResults';
 import SearchBar from './SearchBar'
 import TrackList from './TrackList'
+import Spotify from './Spotify'
 
 
 
 function App(){
+    const [accessToken, setAccessToken] = useState('');
 
-    return <>
+    useEffect(() => {
+        setAccessToken( Spotify.SpotifyAccessToken())
+      },[]); // run the access routine once at the start
+
+    return (
+    <>
         <h1>Fluffy Awesome Spotify List Maker</h1>
+        <p>{accessToken}</p>
         <div id='container'>
             <div id ="searchContainer">
                 <h2>Search</h2>
@@ -23,7 +31,7 @@ function App(){
         </div>
 
 
-    </>
+    </>)
 }
 
 
